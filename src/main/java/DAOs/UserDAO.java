dcb76baed6dde7c161fa093c2cb311b1edcba1b4
Bernardo Estacio Abreu <bernardo.estacio89@gmail.com>
2024-07-28 18:16:41 +0200

    [Task] first commit of full model.

--- src/main/java/DAOs/UserDAO.java
+++ src/main/java/DAOs/UserDAO.java
@@ -1,2 +1,95 @@
-package DAOs;public class UserDAO {
+package DAOs;
+
+import Entities.User;
+import Utilities.DatabaseUtil;
+
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserDAO {
+
+    // Create
+    public void addUser(String name, String email) {
+        String query = "INSERT INTO users (name, email) VALUES (?, ?)";
+        try (Connection conn = DatabaseUtil.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(query)) {
+            stmt.setString(1, name);
+            stmt.setString(2, email);
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Read
+    public User getUser(int id) {
+        String query = "SELECT * FROM users WHERE id = ?";
+        User user = null;
+        try (Connection conn = DatabaseUtil.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(query)) {
+            stmt.setInt(1, id);
+            try (ResultSet rs = stmt.executeQuery()) {
+                if (rs.next()) {
+                    user = new User();
+                    user.setId(rs.getInt("id"));
+                    user.setName(rs.getString("name"));
+                    user.setEmail(rs.getString("email"));
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return user;
+    }
+
+    // Update
+    public void updateUser(int id, String name, String email) {
+        String query = "UPDATE users SET name = ?, email = ? WHERE id = ?";
+        try (Connection conn = DatabaseUtil.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(query)) {
+            stmt.setString(1, name);
+            stmt.setString(2, email);
+            stmt.setInt(3, id);
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Delete
+    public void deleteUser(int id) {
+        String query = "DELETE FROM users WHERE id = ?";
+        try (Connection conn = DatabaseUtil.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(query)) {
+            stmt.setInt(1, id);
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Get All Users
+    public List<User> getAllUsers() {
+        List<User> users = new ArrayList<>();
+        String query = "SELECT * FROM users";
+        try (Connection conn = DatabaseUtil.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(query);
+             ResultSet rs = stmt.executeQuery()) {
+            while (rs.next()) {
+                User user = new User();
+                user.setId(rs.getInt("id"));
+                user.setName(rs.getString("name"));
+                user.setEmail(rs.getString("email"));
+                users.add(user);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return users;
+    }
 }
--- src/main/java/Utilities/DatabaseUtil.java
+++ src/main/java/Utilities/DatabaseUtil.java
@@ -1,2 +1,38 @@
-package Utilities;public class DatabaseUtil {
+package Utilities;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DatabaseUtil {
+
+    private static final String PROPERTIES_FILE = "db.properties";
+    private static String url;
+    private static String user;
+    private static String password;
+    private static String driver;
+
+    static {
+        try (InputStream input = DatabaseUtil.class.getClassLoader().getResourceAsStream(PROPERTIES_FILE)) {
+            Properties properties = new Properties();
+            if (input == null) {
+                throw new IOException("Unable to find " + PROPERTIES_FILE);
+            }
+            properties.load(input);
+            url = properties.getProperty("db.url");
+            user = properties.getProperty("db.user");
+            password = properties.getProperty("db.password");
+            driver = properties.getProperty("db.driver");
+            Class.forName(driver);
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(url, user, password);
+    }
 }
